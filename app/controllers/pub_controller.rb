420fd56b514fffe80c07d42caceeee364dbef966
Ádám Z. Kövér <ardavan.hu@gmail.com>
2014-02-24 08:44:10 +0100

    Changed search results to JSONP

--- app/controllers/pub_controller.rb
+++ app/controllers/pub_controller.rb
@@ -7,7 +7,7 @@
 	def search
 		dictionary = Dictionary.new
 		@entries = dictionary.search params[:term], ""
-		render :json => @entries
+		render :json => @entries, :callback => params[:callback]
 	end
 
 	def suggest
