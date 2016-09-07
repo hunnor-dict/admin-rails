6db0e671ef45fb14e8d1f32a28a5930751ec1bda
Ádám Z. Kövér <adamzkover@gmail.com>
2016-09-07 10:04:12 +0200

    Fix XSL paths

--- app/models/solr.rb
+++ app/models/solr.rb
@@ -6,8 +6,8 @@ class Solr
 		@solr[:nb] = RSolr.connect :url => ENV["SOLR_URL"] + "/hunnor.nb"
 		solr_dir = ENV["SOLR_DIR"]
 		@xsl = {}
-		@xsl[:nb]  = Nokogiri::XSLT(File.open("#{solr_dir}/hunnor-nb/conf/import.xsl", "rb"))
-		@xsl[:hu]  = Nokogiri::XSLT(File.open("#{solr_dir}/hunnor-hu/conf/import.xsl", "rb"))
+		@xsl[:nb]  = Nokogiri::XSLT(File.open("#{solr_dir}/hunnor.nb/conf/import.xsl", "rb"))
+		@xsl[:hu]  = Nokogiri::XSLT(File.open("#{solr_dir}/hunnor.hu/conf/import.xsl", "rb"))
 	end
 
 	def save lang, id, entry, document
