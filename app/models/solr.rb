0a6c61c9a581cbf4ce1375faf876e90a58dfa978
Ádám Z. Kövér <adamzkover@gmail.com>
2017-08-26 22:35:11 +0200

    Send custom XML directly to Solr

--- app/models/solr.rb
+++ app/models/solr.rb
@@ -4,16 +4,11 @@ class Solr
 		@solr = {}
 		@solr[:hu] = RSolr.connect :url => ENV["SOLR_URL"] + "/hunnor.hu"
 		@solr[:nb] = RSolr.connect :url => ENV["SOLR_URL"] + "/hunnor.nb"
-		solr_dir = ENV["SOLR_DIR"]
-		@xsl = {}
-		@xsl[:nb]  = Nokogiri::XSLT(File.open("#{solr_dir}/hunnor.nb/conf/import.xsl", "rb"))
-		@xsl[:hu]  = Nokogiri::XSLT(File.open("#{solr_dir}/hunnor.hu/conf/import.xsl", "rb"))
 	end
 
 	def save lang, id, entry, document
 		if id.to_s == entry.to_s
-			solr_doc = @xsl[lang].transform document
-			data = solr_doc.to_xml(:encoding => "UTF-8", :indent_text => "\t", :indent => 1)
+			data = document.to_xml(:encoding => "UTF-8", :indent_text => "\t", :indent => 1)
 			@solr[lang].update :data => data
 		else
 			@solr[lang].delete_by_id id
