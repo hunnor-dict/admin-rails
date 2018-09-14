8c9008735f213257c461beb7a5273ed47076ac12
Ádám Z. Kövér <adamzkover@gmail.com>
2018-09-14 16:38:51 +0200

    Specify callback_url with https://

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,7 +1,12 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
-	provider :facebook, ENV["OMNIAUTH_FACEBOOK_KEY"], ENV["OMNIAUTH_FACEBOOK_SECRET"]
+
+	provider :facebook, ENV["OMNIAUTH_FACEBOOK_KEY"], ENV["OMNIAUTH_FACEBOOK_SECRET"], client_options: {
+		callback_url: "https://hunnor-dict-admin.ovitas.no/auth/facebook/callback"
+	}
+
 	provider :google_oauth2, ENV["OMNIAUTH_GOOGLE_KEY"],ENV["OMNIAUTH_GOOGLE_SECRET"], {access_type: 'online', approval_prompt: ''}
+
 	provider :linkedin, ENV["OMNIAUTH_LINKEDIN_KEY"], ENV["OMNIAUTH_LINKEDIN_SECRET"]
+
 	provider :twitter, ENV["OMNIAUTH_TWITTER_KEY"], ENV["OMNIAUTH_TWITTER_SECRET"]
 end
-
