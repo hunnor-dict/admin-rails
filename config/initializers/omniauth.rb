521da1adef1c21c9cc74670b0741a49397ffa1d8
Ádám Z. Kövér <adamzkover@gmail.com>
2019-09-23 23:16:48 +0200

    Update Facebook callback URL

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,7 +1,7 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
 
 	provider :facebook, ENV["OMNIAUTH_FACEBOOK_KEY"], ENV["OMNIAUTH_FACEBOOK_SECRET"],
-		callback_url: "https://hunnor-dict-admin.ovitas.no/auth/facebook/callback"
+		callback_url: "https://dict-admin.hunnor.net/auth/facebook/callback"
 
 	provider :linkedin, ENV["OMNIAUTH_LINKEDIN_KEY"], ENV["OMNIAUTH_LINKEDIN_SECRET"]
 
